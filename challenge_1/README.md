897a7c8f8211832a7040d103da98a894e408a9f1
Matt Violet <mattviolet@gmail.com>
2019-06-25 15:25:25 -0700

    Setup webpack

---
+++ challenge_1/README.md
@@ -0,0 +1,12 @@
+npm install --save
+npm init
+npm install react --save
+npm install react-dev --save
+npm install --save-dev webpack
+npm install --save-dev webpack-cli
+npm install babel-cli babel-core --save-dev
+npm install babel-preset-es2015 --save-dev
+npm install babel-preset-react
+npm install body-parser
+npm install react-dom
+npm install -g json-server
---
+++ challenge_1/webpack.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  entry: __dirname + "/client/app.jsx",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: [ "babel-loader"],
+      }, 
+      {
+        test: /\.css$/,
+        use: [{loader: 'style-loader'}, 
+        {loader: 'css-loader',
+        options: {
+          modules: true,
+          importLoaders: 1,
+          localIdentName: '[sha1:hash:hex:6]',
+        },}],
+      }
+    ]
+  },
+  output: {
+    filename: 'bundle.js',
+    path: __dirname + '/client/'
+  }
+};
